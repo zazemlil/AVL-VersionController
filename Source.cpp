5b0a7af3fd94c508acb069d3fd07e848376090c6
jetZ666 <tiknik19@mail.ru>
2024-09-17 12:48:48 +0300

    -

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,13 @@
 #include "Tree.h"
+#include <cstdlib>
 
 int main() {
 	Tree* tree = new Tree(1);
 	Tree::insert(tree->getHead(), 3);
-	Tree::insert(tree->getHead(), 2);
+	Tree::insert(tree->getHead(), -2);
 
 	Tree::deepShow(tree->getHead());
+
+	std::system("Pause");
 	return 0;
 }
